1c357c37234704ff13404ed9577b85f1a036bea2
HSC-1 <kreaoo13@gmail.com>
2021-12-29 20:18:45 +0900

    최신 버전만 있음  gitignore 확인

---
+++ TEST_Simulation_03/README.md
@@ -0,0 +1,57 @@
+# PythonQT-TEST_Simulation_02
+
+
+## ê°„ëµ???¤ëª…
+-   json ??png(?¼ë²¨) ë³€??ê¸°ëŠ¥
+-   png ?‰ìƒ ? íƒ ê¸°ëŠ¥
+-   json annotation ?´ë˜?¤ë³„ ? íƒ ê¸°ëŠ¥
+
+
+## 3ì¢…ê¸°??
+<img src = "https://i.imgur.com/bSzUcyI.gif" width="400">      
+
+
+
+## 1.DATA SPOON
+
+<img src = "https://i.imgur.com/oPhMYA3.png" width="400">      
+
+## 2. Simulation settings
+       
+<img src = "https://i.imgur.com/wAW3GAo.png" width="400">           
+
+
+## 2. Visualization
+      
+<img src = "https://github.com/SIMYJ/pythonQT/blob/syj/image/%EC%84%B1%EB%8A%A5%EC%A7%80%ED%91%9C_%EC%98%81%EC%83%81.gif?raw=true" width="400">           
+
+
+
+
+
+
+## ?„ë¡œ?íŠ¸ êµ¬ì„±
+
+
+```txt
+/pythonQT
+	/? ì?_?¼ë³µì§€????³µ?„ì„±_?´ë?ì§€_ê°•ì›_ë°?ì¶©ì²­
+		      
+          /1.?¼ë²¨ë§ë°?´í„° 
+	  	/2.??³µ?¬ì§„_Fine_1024?½ì?
+                    /1.Ground_Truth_Tiff                  # ?ë˜ tif?Œì¼ë§?ì¡´ì¬?ˆìŒ + ?ë³¸?´ë?ì§€???€??png?Œì¼ ?„ì˜ë¡? ë§Œë“¬
+	            /2.Ground_Truth_JSON_?„ì²´              # ?„ì²˜ë¦¬ì— ?¬ìš©??JSON?Œì¼ ?”ë ‰? ë¦¬
+                    /3.Ground_Truth_JSON_??ª©ë³?             # ?œìš© ?ˆí•¨
+                    /4.ë©”í??°ì´??                          # ë©”í??°ì´??(coordinates ê³„ì‚°???„í•´ ?¬ìš©??
+                    /5.Ground_Truth_PNG_?„ì²´               # ?„ì²˜ë¦??‘ì—… ???€?¥ê²½ë¡??”ë ‰? ë¦¬
+				
+	/TEST_Simulation_02
+		/json2png_EDA.py          # JSON-> PNG?„ì²˜ë¦??Œì¼
+		/Sim2data.ui              # UI?Œì¼
+		/Sim2data.ui.py           # UIêµ¬í˜„ ì½”ë“œ
+		/making_label.py          # ?¬ìš©?ˆí•¨(??TEST_01ì½”ë“œ)
+```
+
+
+
+
---
+++ TEST_Simulation_03/bbox.py
@@ -0,0 +1,186 @@
+import os , glob
+import cv2
+import numpy as np
+from PIL import Image
+import math
+import json
+
+
+# 파이썬 디렉토리 처리 총정리
+# https://ddolcat.tistory.com/654
+
+
+class yolo2voc:
+
+    def __init__(self):
+        #리눅스코드 os.chdir("../")
+        #리눅스코드 self.path =os.getcwd()
+        #print("기본경로: {} ".format(self.path))
+        self.path = os.path.os.getcwd()
+        print("yolo2voc_기론경로{}".format(self.path))
+
+        self.label_dict = {}
+
+        # #json 폴더 경로
+        # self.path_txt_dir = "/home/aiffel-dj16/sim2data/pythonQT/bbox/label"
+        # print("txt 폴더경로 : {}".format(self.path_txt_dir))
+
+        # #read json folder
+        # list_file = os.listdir(self.path_txt_dir)
+        # self.list_file_name = [ file for file in list_file if file.endswith(".txt")]
+        # self.list_file_name.sort() # 정렬
+        # print("txt파일명 리스트출력 : {}".format(self.list_file_name))
+
+
+    def yolo2coco(self, image_dir_path, label_dir_path, save_file_name, is_normalized):
+
+        total = {}
+
+        # make info
+        info = {
+                'description' : '',
+                'url' : '',
+                'version' : '',
+                'year' : 2020,
+                'contributor' : '',
+                'data_created' : '2020-04-14 01:45:18.567988'
+                }
+        total['info'] = info
+
+        # make licenses
+        licenses_list = []
+        licenses_0= {
+                'id' : '1',
+                'name' : 'your_name',
+                'url' : 'your_name'
+                }
+        licenses_list.append(licenses_0)
+
+        ''' if you want to add licenses, copy this code
+        licenses_1 = {
+            'id': '2',
+            'name': 'your_name',
+            'url': 'your_name'
+        }
+        licenses_list.append(licenses_1)
+        '''
+
+        total['licenses'] = licenses_list
+
+        # make categories
+        category_list = []
+        class_0 = {
+                'id':  1,
+                'name' : 'defect',
+                'supercategory' : 'None'
+                }
+        category_list.append(class_0)
+
+        '''
+        # if you want to add class
+        class_1 = {
+                'id':  2,
+                'name' : 'defect',
+                'supercategory' : 'None'
+                }
+        category_list.append(class_1)
+        '''
+        total['categories'] = category_list
+
+        # make yolo to coco format
+
+        # get images
+        image_list = os.listdir(image_dir_path)
+        print('image length : ', len(image_list))
+        label_list = os.listdir(label_dir_path)
+        print('label length : ',len(label_list))
+
+        image_dict_list = []
+        count = 0
+        for image_name in image_list :
+            img = cv2.imread(image_dir_path+image_name)
+            image_dict = {
+                    'id' : count,
+                    'file_name' : image_name,
+                    'width' : img.shape[1],
+                    'height' : img.shape[0],
+                    'date_captured' : '2020-04-14 -1:45:18.567975',
+                    'license' : 1, # put correct license
+                    'coco_url' : '',
+                    'flickr_url' : ''
+                    }
+            image_dict_list.append(image_dict)
+            count += 1
+        total['images'] = image_dict_list
+
+        # make yolo annotation to coco format
+        label_dict_list = []
+        image_count = 0
+        label_count = 0
+        for image_name in image_list :
+            img = cv2.imread(image_dir_path+image_name)
+            label = open(label_dir_path+image_name[0:-4] + '.txt','r')
+            if not os.path.isfile(label_dir_path + image_name[0:-4] + '.txt'): # debug code
+                print('there is no label match with ',image_dir_path + image_name)
+                return
+            while True:
+                line = label.readline()
+                if not line:
+                    break
+                class_number, center_x,center_y,box_width,box_height = line.split()
+                # should put bbox x,y,width,height
+                # bbox x,y is top left
+
+                if is_normalized :
+                    center_x =  int(float(center_x) * int(img.shape[1]))
+                    center_y = int(float(center_y) * int(img.shape[0]))
+                    box_width = int(float(box_width) * int(img.shape[1]))
+                    box_height = int(float(box_height) * int(img.shape[0]))
+                    top_left_x = center_x - int(box_width/2)
+                    top_left_y = center_y - int(box_height/2)
+
+                if not is_normalized :
+                    center_x = float(center_x)
+                    center_y = float(center_y)
+                    box_width = float(box_width)
+                    box_height = float(box_height)
+                    top_left_x = center_x - int(box_width / 2)
+                    top_left_y = center_y - int(box_height / 2)
+
+                bbox_dict = []
+                bbox_dict.append(top_left_x)
+                bbox_dict.append(top_left_y)
+                bbox_dict.append(box_width)
+                bbox_dict.append(box_height)
+
+                # segmetation dict : 8 points to fill, x1,y1,x2,y2,x3,y3,x4,y4
+                segmentation_list_list = []
+                segmentation_list= []
+                segmentation_list.append(bbox_dict[0])
+                segmentation_list.append(bbox_dict[1])
+                segmentation_list.append(bbox_dict[0] + bbox_dict[2])
+                segmentation_list.append(bbox_dict[1])
+                segmentation_list.append(bbox_dict[0]+bbox_dict[2])
+                segmentation_list.append(bbox_dict[1]+bbox_dict[3])
+                segmentation_list.append(bbox_dict[0])
+                segmentation_list.append(bbox_dict[1] + bbox_dict[3])
+                segmentation_list_list.append(segmentation_list)
+
+                label_dict = {
+                        'id' : label_count,
+                        'image_id' : image_count,
+                        'category_id' : int(class_number)+1,
+                        'iscrowd' : 0,
+                        'area' : int(bbox_dict[2] * bbox_dict[3]),
+                        'bbox' : bbox_dict,
+                        'segmentation' : segmentation_list_list
+                        }
+                label_dict_list.append(label_dict)
+                label_count += 1
+            label.close()
+            image_count += 1
+
+        total['annotations'] = label_dict_list
+
+        with open(save_file_name,'w',encoding='utf-8') as make_file :
+            json.dump(total,make_file, ensure_ascii=False,indent='\t')
